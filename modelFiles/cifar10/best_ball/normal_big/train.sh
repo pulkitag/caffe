7a11589688af3284715e7ac2a5e2a50f6923ffb7
pulkitag <pulkitag@anakin>
2014-10-26 07:35:26 -0700

    Added gitignore

--- modelFiles/cifar10/best_ball/normal_big/train.sh
+++ modelFiles/cifar10/best_ball/normal_big/train.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh 
 TOOLS=/work4/pulkitag-code/pkgs/caffe-v2-2//build/tools 
-LOG_FILE_NAME=/work4/pulkitag-code/pkgs/caffe-v2-2/modelFiles/cifar10/best_ball/normal_big/logs/round1_lr0.00025000.txt 
+LOG_FILE_NAME=/work4/pulkitag-code/pkgs/caffe-v2-2/modelFiles/cifar10/best_ball/normal_big/logs/round1_lr0.00100000.txt 
  
 GLOG_logtostderr=1 $TOOLS/caffe train --solver=/work4/pulkitag-code/pkgs/caffe-v2-2/modelFiles/cifar10/best_ball/normal_big/solver.prototxt  2>&1 | tee -a ${LOG_FILE_NAME}
