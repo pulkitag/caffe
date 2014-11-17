bd7f4f0f33ccf78394e5fa4c162e99a88fa66cf1
pulkitag <pulkitag@berkeley.edu>
2014-11-17 14:52:15 -0500

    Added Diagonal Accurcy Layer

---
+++ src/caffe/layers/accuracy_diagonal_layer.cpp
@@ -0,0 +1,96 @@
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <vector>
+
+#include "caffe/layer.hpp"
+#include "caffe/util/io.hpp"
+#include "caffe/util/math_functions.hpp"
+#include "caffe/vision_layers.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void AccuracyDiagonalLayer<Dtype>::LayerSetUp(
+  const vector<Blob<Dtype>*>& bottom, vector<Blob<Dtype>*>* top) {
+  top_k_ = this->layer_param_.accuracy_param().top_k();
+	CHECK_EQ(top_k_, 1) << "Diagonal Accuracy only supports top_k = 1";
+
+  int dim = bottom[0]->count() / bottom[0]->num();
+	num_class_ = dim;
+	for (int j=0; j < dim; j++){
+		class_hit_.push_back(0);
+		class_count_.push_back(0);
+	}
+
+}
+
+//Unfortuante nameof the function - its for printing debug info
+template <typename Dtype>
+void AccuracyDiagonalLayer<Dtype>::PrintWeights() const {
+	LOG(INFO)<<"Class-Wise Acc ";
+	for (int i=0; i<num_class_; i++){
+		Dtype acc;
+		if (class_count_[i] > 0)
+			acc = ((Dtype)class_hit_[i])/(Dtype)class_count_[i];
+		else
+			acc =  -1;
+		LOG(INFO) << "Class " << i << " acc:  " << acc;
+	}
+}
+template <typename Dtype>
+void AccuracyDiagonalLayer<Dtype>::Reshape(
+  const vector<Blob<Dtype>*>& bottom, vector<Blob<Dtype>*>* top) {
+  CHECK_EQ(bottom[0]->num(), bottom[1]->num())
+      << "The data and label should have the same number.";
+  CHECK_LE(top_k_, bottom[0]->count() / bottom[0]->num())
+      << "top_k must be less than or equal to the number of classes.";
+  CHECK_EQ(bottom[1]->channels(), 1);
+  CHECK_EQ(bottom[1]->height(), 1);
+  CHECK_EQ(bottom[1]->width(), 1);
+  (*top)[0]->Reshape(1, 1, 1, 1);
+}
+
+template <typename Dtype>
+void AccuracyDiagonalLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+    vector<Blob<Dtype>*>* top) {
+  Dtype accuracy = 0;
+  const Dtype* bottom_data = bottom[0]->cpu_data();
+  const Dtype* bottom_label = bottom[1]->cpu_data();
+  int num = bottom[0]->num();
+  int dim = bottom[0]->count() / bottom[0]->num();
+  vector<Dtype> maxval(top_k_+1);
+  vector<int> max_id(top_k_+1);
+  for (int i = 0; i < num; ++i) {
+    // Top-k accuracy
+    std::vector<std::pair<Dtype, int> > bottom_data_vector;
+    for (int j = 0; j < dim; ++j) {
+      bottom_data_vector.push_back(
+          std::make_pair(bottom_data[i * dim + j], j));
+    }
+    std::partial_sort(
+        bottom_data_vector.begin(), bottom_data_vector.begin() + top_k_,
+        bottom_data_vector.end(), std::greater<std::pair<Dtype, int> >());
+    // check if true label is in top k predictions
+    for (int k = 0; k < top_k_; k++) {
+      if (bottom_data_vector[k].second == static_cast<int>(bottom_label[i])) {
+        ++class_hit_[static_cast<int>(bottom_label[i])];
+        break;
+      }
+    }
+		++class_count_[static_cast<int>(bottom_label[i])];
+  }
+
+	for (int j=0; j < dim; ++j){
+		if (class_count_[j] > 0)
+			accuracy += ((Dtype)class_hit_[j])/(Dtype)class_count_[j];
+	}
+
+  // LOG(INFO) << "Accuracy: " << accuracy;
+  (*top)[0]->mutable_cpu_data()[0] = accuracy / dim;
+  // Accuracy layer should not be used as a loss function.
+}
+
+INSTANTIATE_CLASS(AccuracyDiagonalLayer);
+
+}  // namespace caffe
