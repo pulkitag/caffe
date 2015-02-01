Checked and corrected the rotation code

modelFiles/keypoints/code/prepare_data.py
@@ -174,6 +174,10 @@ def get_angle_counts(fileName, thetaThresh):
 
 
 def find_view_centers(views, idxs, numCenters, maxAngle=30):
+	'''
+		Find the centers by only taking examples which correspond to a rotation
+		of less than maxAngle.
+	'''
 	numSample  = 50000
 	nSPerClass = int(numSample/len(idxs))
 	viewDiff   = np.zeros((numSample,3,3))
@@ -203,7 +207,6 @@ def find_view_centers(views, idxs, numCenters, maxAngle=30):
 
 	return centers
 	
-
 
 def create_data_labels(h5LabelFile, views, idxs, labelType, viewCenters=[]):
 	numSamples = [len(i) for i in idxs]
@@ -239,7 +242,8 @@ def create_data_labels(h5LabelFile, views, idxs, labelType, viewCenters=[]):
 		angRange    = np.pi*(angRange/180.)
 		if len(viewCenters)==0:
 			#Need to find the medoids/centers.
-			viewCenters = find_view_centers(views, idxs, numLabels-1)
+			print "Computing Centers ..."
+			viewCenters = find_view_centers(views, idxs, numLabels-1, maxAngle=30)
 	else:
 		print "Unrecognized labelType "
 		raise("Label Type not found exception")
@@ -274,8 +278,12 @@ def create_data_labels(h5LabelFile, views, idxs, labelType, viewCenters=[]):
 					pdb.set_trace()			
 			elif labelType =='kmedoids30_20':
 				viewDiff = np.dot(view2, np.linalg.inv(view1))
-				viewDiff = ru.get_cluster_assignments(viewDiff, viewCenters)	
-	
+				theta,_  = ru.rotmat_to_angle_axis(viewDiff)
+				if theta < ((np.pi)*30.0)/180.0:
+					viewDiff,_ = ru.get_cluster_assignments(viewDiff.reshape((1,3,3)), viewCenters)	
+				else:
+					viewDiff = np.array([20])
+
 			labels[lSt:lEn] = viewDiff.flatten()
 			indices[count:count+4] = idx,cl,clIdx1,clIdx2
 			count = count + 4
@@ -362,6 +370,40 @@ def h52db(exp, labelType, imSz, lblOnly=False):
 		h5LbName = get_lblH5Name(s, exp, imSz, labelType)
 		dbLbName = get_lblDbName(s, exp, imSz, labelType)	 
 		subprocess.check_call(['%s %s %s %d' % (lbToolName, h5LbName, dbLbName, labelSz)],shell=True)
+
+
+def vis_rot_clusters():
+	exp       = 'rigid'
+	labelType = 'kmedoids30_20'  
+	vcFileName  = get_view_centers_name(exp, labelType)
+	viewCenters = read_view_centers(vcFileName)
+	
+	outFile = 'data/exp%s_lbl%s_clusters.mat' % (exp, labelType)
+	N       = viewCenters.shape[0]
+	centers = np.zeros((N,3))
+	
+	for i in range(0,N):
+		theta,v = ru.rotmat_to_angle_axis(viewCenters[i])
+		centers[i] = theta*v
+
+	sio.savemat(outFile, {'centers':centers})	
+
+
+def vis_im_label_pairs():
+	'''
+		Stores the image and label pairs for visualization.
+		USeful for sanity check that the leveldb which was formed
+		along with the labels was sane. 
+	'''
+	imSz      = 128
+	exp       = 'rigid'
+	labelType = 'kmedoids30_20' 
+	setName   = 'val'	
+
+	#File Names
+	h5ImName = get_imH5Name(setName, exp, imSz)
+	h5LbName = get_lblH5Name(setName, exp, imSz, labelType)
+
 
 
 if __name__ == "__main__":

modelFiles/keypoints/code/rot_utils.py
@@ -4,9 +4,6 @@ from   scipy import linalg as linalg
 import sys, os
 import pdb
 import math
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
@@ -18,8 +15,8 @@ def get_rot_angle(view1, view2):
 		pdb.set_trace()
 
 	viewDiff = linalg.norm(viewDiff, ord='fro')
-	assert not np.isnan(viewDiff.flatten())
-	assert not np.isinf(viewDiff.flatten())
+	assert not any(np.isnan(viewDiff.flatten()))
+	assert not any(np.isinf(viewDiff.flatten()))
 	angle    = viewDiff/np.sqrt(2)
 	return angle
 
@@ -28,14 +25,13 @@ def get_cluster_assignments(x, centers):
 	N       = x.shape[0]
 	nCl     = centers.shape[0]	
 	distMat = np.inf * np.ones((nCl,N))
-	print "Num Examples: %d, Num Dist-Mat: %d" % (N, nCl)
 
 	for c in range(nCl):
 		for i in range(N):
 			distMat[c,i] = get_rot_angle(centers[c], x[i])
 
-	assert not np.isinf(distMat.flatten())
-	assert not np.isnan(distMat.flatten())
+	assert not any(np.isinf(distMat.flatten()))
+	assert not any(np.isnan(distMat.flatten()))
 	
 	assgn    = np.argmin(distMat, axis=0)
 	minDist  = np.amin(distMat, axis=0)
@@ -156,32 +152,89 @@ def rotmat_to_angle_axis(rotMat):
 	v1,v2,v3 = -aa[1,2], aa[0,2], -aa[0,1]
 	v  = np.array((v1,v2,v3))
 	theta = np.linalg.norm(v)
-	v     = v/theta
+	if theta>0:
+		v     = v/theta
 	return theta, v
 
 
-def plot_rotmats(rotMats):
+def plot_rotmats(rotMats, isInteractive=True):
+	if isInteractive:
+		import matplotlib
+		matplotlib.use('tkagg')
+		import matplotlib.pyplot as plt
+	else:
+		import matplotlib
+		matplotlib.use('Agg')
+		import matplotlib.pyplot as plt
+	
+	N = rotMats.shape[0]
 	plt.ion()
 	fig = plt.figure()
 	ax = fig.add_subplot(111, projection='3d')
-	xpos, ypos, zpos = [0], [0], [0]	
-	N = rotMats.shape[0]
+	
+	xpos, ypos, zpos = np.zeros((N,1)), np.zeros((N,1)), np.zeros((N,1))
+	vx,vy,vz = [],[],[]
+
 	for i in range(N):
 		theta,v = rotmat_to_angle_axis(rotMats[i])
 		v       = theta * v
-		ax.quiver(xpos,ypos,zpos,v[0],v[1],v[2])
-	
-	plt.draw()
+		vx.append(v[0])
+		vy.append(v[1])
+		vz.append(v[2])
+
+	ax.quiver(xpos,ypos,zpos,vx,vy,vz)
+	plt.show()	
+	ax.set_xlim(-1,1)
+	ax.set_ylim(-1,1)
+	ax.set_zlim(-1,1)
 
 
-def generate_random_rotmats(numMat = 100, thetaRange=np.pi/4):
+def generate_random_rotmats(numMat = 100, thetaRange=np.pi/4, thetaFixed=False):
 	rotMats = np.zeros((numMat,3,3))
-	#Randomly generate an axis for rotation matrix
-	v    = np.random.random(3)
-	for i in range(numMat):
-		theta      = thetaRange * np.random.random()					
-		rotMats[i] = angle_axis_to_rotmat(theta, v)
+
+	if not thetaFixed:
+		#Randomly generate an axis for rotation matrix
+		v    = np.random.random(3)
+		for i in range(numMat):
+			theta      = thetaRange * np.random.random()					
+			rotMats[i] = angle_axis_to_rotmat(theta, v)
+	else:
+		for i in range(numMat):
+			v    = np.random.randn(3)
+			v    = v/linalg.norm(v)
+			theta      = thetaRange * np.random.random()					
+			rotMats[i] = angle_axis_to_rotmat(theta, v)
+		
 	return rotMats
 
 
+def test_clustering():
+	'''
+	For testing clustering:
+	Randomly generate soem data, cluster it and save it .mat file
+	Using matlab I will then visualize it. Visualizing in python is being a pain. 
+	'''
+	N   = 1000
+	nCl = 3
+
+	#Generate the data using nCl different axes. 
+	dat = np.zeros((N,3,3))
+	idx = np.linspace(0,N,nCl+1).astype('int')
+	for i in range(nCl):
+		dat[idx[i]:idx[i+1]] = generate_random_rotmats(idx[i+1]-idx[i],thetaFixed=True)	
+
+	assgn, centersMat = cluster_rotmats(dat,nCl)
+
+	points = np.zeros((N,3))
+	for i in range(N):
+		theta,points[i] = rotmat_to_angle_axis(dat[i])
+		points[i] = theta*points[i]
+
+	centers = np.zeros((nCl,3))
+	for i in range(nCl):
+		theta,centers[i] = rotmat_to_angle_axis(centersMat[i])
+		centers[i] = theta*centers[i]
+
+	sio.savemat('test_clustering.mat',{'assgn':assgn,'centers':centers,'points':points})	 
+	
 	