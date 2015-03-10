20a90049e96f45d1bc50420c0efdf5812105d5c5
pulkitag <pulkitag@berkeley.edu>
2015-03-10 16:43:20 -0700

    Added ILSVRC12 Reader

--- python/my_pycaffe_utils.py
+++ python/my_pycaffe_utils.py
@@ -2,6 +2,8 @@ import my_pycaffe as mp
 import my_pycaffe_io as mpio
 import numpy as np
 import pdb
+import os
+import scipy.io as sio
 
 def zf_saliency(net, imBatch, numOutputs, opName, ipName='data', stride=2, patchSz=11):
 	'''
@@ -55,4 +57,71 @@ def zf_saliency(net, imBatch, numOutputs, opName, ipName='data', stride=2, patch
 	return imSalient, origScore
 
 
-	
+def mapILSVRC12_labels_wnids(metaFile):
+	dat    = sio.loadmat(metaFile, struct_as_record=False, squeeze_me=True)
+	dat    = dat['synsets']
+	labels, wnid = [],[]
+	for dd in dat:
+		labels.append(dd.ILSVRC2012_ID - 1)
+		wnid.append(dd.WNID)
+	labels = labels[0:1000]
+	wnid   = wnid[0:1000]	
+	return labels, wnid
+
+
+class ILSVRC12Reader:
+	def __init__(self, caffeDir='/work4/pulkitag-code/pkgs/caffe-v2-2'):
+		labelFile  = '/data1/pulkitag/ILSVRC-2012-raw/devkit-1.0/data/ILSVRC2012_validation_ground_truth.txt'
+		metaFile      = '/data1/pulkitag/ILSVRC-2012-raw/devkit-1.0/data/meta.mat'
+		self.imFile_     = '/data1/pulkitag/ILSVRC-2012-raw/256/val/ILSVRC2012_val_%08d.JPEG'
+		self.count_      = 0
+
+		#Load the groundtruth labels from Imagenet
+		fid  = open(labelFile)
+		data = fid.readlines()
+		valLabels = [int(i)-1 for i in data] #-1 for python formatting 
+		
+		#Load the Synsets
+		synFile = os.path.join(caffeDir, 'data/ilsvrc12/synsets.txt')
+		fid     = open(synFile, 'r')
+		self.synsets_ = [s.strip() for s in fid.readlines()]
+		fid.close()
+
+		#Align the Imagenet Labels to the Synset order on which the BVLC reference model was trained.
+		modLabels, modWnid = mapILSVRC12_labels_wnids(metaFile)
+		self.labels_ = np.zeros((len(valLabels),)).astype(np.int)
+		for (i,lb) in enumerate(valLabels):
+			lIdx = modLabels.index(lb)
+			syn  = modWnid[lIdx]
+			sIdx = self.synsets_.index(syn)
+			self.labels_[i] = sIdx
+
+		#Load the synset words
+		synWordFile = os.path.join(caffeDir, 'data/ilsvrc12/synset_words.txt')
+		fid         = open(synWordFile, 'r')
+		data        = fid.readlines()
+		self.words_ = {}
+		for l in data:
+			synNames = l.split()
+			syn      = synNames[0]
+			words    = [w for w in synNames[1:]]
+			self.words_[syn] = words
+		fid.close()
+
+
+	def reset(self):
+		self.count_ = 0
+
+	def set_count(self, count):
+		self.count_ = count
+
+	def read(self):
+		imFile = self.imFile_ % (self.count_ + 1)
+		im     = mp.caffe.io.load_image(imFile)
+		lb     = self.labels_[self.count_]	
+		syn    = self.synsets_[lb]
+		words  = self.words_[syn]
+		self.count_ += 1
+		return im, lb, syn, words
+
+			
